7a4f677b7660b5606792d2c553501cf00d936bf4
sudharshanreddykr <sudharshanreddykr@gmail.com>
2021-05-17 09:41:20 +0530

    8th commit

--- src/containers/Cart.tsx
+++ src/containers/Cart.tsx
@@ -1,46 +1,34 @@
 import axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
-import { RouteComponentProps } from "react-router";
 import Column from "../components/Column";
-import StorageService from "../services/StorageService";
-import { CartType } from "../types";
-import { BrowserRouter, Link, NavLink, Redirect, useHistory } from "react-router-dom";
+import { CartType, StoreType } from "../types";
+import {
+  NavLink,
+  Redirect,
+  RouteComponentProps,
+} from "react-router-dom";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import { Dispatch } from "redux";
 import CartActions from "../store/actions/CartActions";
-import ImageWithFallback from "../components/ImageWithFallback";
+
 type Props = {
-  cartItems: any;
-  btnClick: () => void;
+  cartItems: CartType[];
   deleteCartData: (id: number) => void;
   increamentQty: (id: number) => void;
   decrementQty: (id: number) => void;
 } & RouteComponentProps;
 type State = {
-  change: boolean;
   reRender: boolean;
   totalAmo: number;
 };
+
 class Cart extends React.Component<Props, State> {
-  state: State = { change: false, reRender: false, totalAmo: 0 };
+  state: State = { reRender: false, totalAmo: 0 };
 
-  deductTotal(price: string) {
-    const temp: number = parseInt(price);
-    this.setState((prevState) => ({ totalAmo: prevState.totalAmo - temp }));
-  }
-  render () {
-        const AllproductId: any = [];
-    let allDataList: any = [];
-    const datas = this.props.cartItems.cart;
-    let finaldata = datas.map((data: any, index: number, arr: any) => {
-      if (AllproductId.includes(data.productId) === false) {
-        allDataList.push(data);
-        AllproductId.push(data.productId);
-      }
-    });
-
+  render() {
+    console.log("total", this.state.totalAmo);
     const submit = (e: any) => {
       e.preventDefault();
       this.setState({
@@ -49,80 +37,119 @@ class Cart extends React.Component<Props, State> {
     };
     const redirecting = () => {
       if (this.state.reRender === true) {
-        return <Redirect to="/payment" />;
+        return <Redirect to="/checkout" />;
       }
     };
 
-    let allTotalAmount: number = 0;
+    let TotalAmount: number = 0;
     return (
       <Container>
         <Row>
-          <Column size={12}>
-            <div className="jumbotron text-center">
-              <h1 className="display-5 fw-bold">Cart Item</h1>
-            </div>
+          <Column size={8}>
+            <div className="jumbotron text-center"></div>
           </Column>
         </Row>
-        <Row>
-          { allDataList.map( ( data: any, index: number ) =>
-            data.productQty > 0 ? (
-             <Column
-                size={ 6 }
-                classes={
-                  "justify-content-between rounded-3 align-items-start rounded-3 mt-1 ms-2 shadow-lg h-auto w-auto mb-3  border border-5"
-                }
-              >
 
-              </Column>
-            )) }
-          <Row>
-            <Column size={6}>
-              <br />
-              <br />
-              <div className="card d-flex border-warning  mb-5 shadow-lg bg-light fw-bold ">
-                <div className="card-header text-center text-dark fs-3">
-                  Total Amount
-                </div>
-                <div className="card-body text-dark w-50">
-                  <p className="card-title">
-                    Sub-Total =
-                    <span className="text-dark fs-5">
-                      &#8377; {allTotalAmount}
-                    </span>
-                  </p>
-                  <hr />
-                  <p className="card-title">
-                    Tax =<span className="text-dark fs-5">&#8377; 00.00</span>
-                  </p>
-                  <hr />
-                  <p className="card-title">
-                    Total =
-                    <span className="text-dark fs-3">
-                      &#8377; {allTotalAmount}
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <NavLink to={"/payment"}>
-                <button
-                  onClick={() => console.log("checkout")}
-                  className="bg-warning border border-3 rounded-3  fw-bold  fs-3 text-light text-center p-1 ms-5 w-75 align-items-center shadow-lg float-center fa fa-shopping-cart"
-                  aria-hidden="true"
-                >
-                  .Check Out
-                </button>
-              </NavLink>
-            </Column>
-          </Row>
-        </Row>
+        <Column size={12}>
+          <div className="container card col-md-8">
+            {redirecting()}
+            <h1 className="display-5 fw-bold text-primary text-center ">
+              CART LIST
+            </h1>
+            <table className="table">
+              <tbody>
+                {this.props.cartItems.map((data: any, index: number) =>
+                  data.productQty > 0 ? (
+                    <tr key={data.productId}>
+                      <div className="">
+                        <td>
+                          <img
+                            src={data.productImage}
+                            className="col-md-3"
+                            alt="img"
+                          />
+                        </td>
+                      </div>
+                      <th className="fw-bold display-7" scope="row">
+                        {index + 1}
+                      </th>
+                      <td className="fw-bold display-7">{data.productId}</td>
+                      <td className="fw-bold display-7">{data.productName}</td>
+                      <td className="fw-bold display-7">
+                        INR {data.productSalePrice}
+                      </td>
+                      <td>
+                        <button
+                          className="btn btn-success m-1"
+                          onClick={() =>
+                            this.props.increamentQty(data.productId)
+                          }
+                        >
+                          +
+                        </button>
+                        {data.productQty}
+                        <button
+                          className="btn btn-danger m-1"
+                          onClick={() =>
+                            this.props.decrementQty(data.productId)
+                          }
+                        >
+                          -
+                        </button>
+                      </td>
+                      <td className="fw-bold display-7">
+                        INR {data.productSalePrice * data.productQty}
+                        <p style={{ display: "none" }}>
+                          {
+                            (TotalAmount =
+                              TotalAmount +
+                              data.productSalePrice * data.productQty)
+                          }
+                        </p>
+                      </td>
+
+                      <td>
+                        <div className="mt-5  pb-0 mb-1  rounded ">
+                          <button
+                            className="btn btn-info fw-bold"
+                            onClick={() => {
+                              this.props.deleteCartData(data.productId);
+                            }}
+                          >
+                            <i className="fas fa-trash display-7"></i>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ) : null
+                )}
+              </tbody>
+            </table>
+            <div className="card-body border border-5 w-50 bg-secondary">
+              <h5 className={"totalProductPrice fw-bold"}>
+                Sub-Total Price: <b className="text-light"> {TotalAmount}</b>
+              </h5>
+            </div>
+            <br />
+            <NavLink to="/payment">
+              <button
+                className="bg-warning w-50 fw-bold border border-5 border-warning text-light p-1 rounded-3 "
+                onClick={submit}
+              >
+                CHECK OUT
+              </button>
+            </NavLink>
+            <br />
+          </div>
+        </Column>
       </Container>
     );
   }
 }
 
-const mapStoreToProps = (store: CartType) => {
+const mapStoreToProps = (state: StoreType) => {
   return {
-    cartItems: store,
+    cartItems: state.cart,
   };
 };
 const mapDispatchToProps = (dispatch: Dispatch) => {
